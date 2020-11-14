b13b5d666e81e2bcb83fec5d447b51becf680737
jingleizhang <eric@xdefi.com>
2020-11-14 12:00:09 +0800

    bug fix

--- migrations/3_deploy_NsureCallToken.js
+++ migrations/3_deploy_NsureCallToken.js
@@ -1,8 +1,9 @@
-const OptionFunction = artifacts.require("OptionFunction");
+const NsureCallToken = artifacts.require("NsureCallToken");
 const WETHToken = artifacts.require("MockToken");
 const DAIToken = artifacts.require("MockToken");
 const Storage = artifacts.require("Storage");
 
+var XCORE_KOVAN = "0x7042758327753f684568528d5eAb0CD2839c6698";
 module.exports = async function (deployer, network) {
     await deployer.deploy(Storage);
 
@@ -10,6 +11,16 @@ module.exports = async function (deployer, network) {
         await deployer.deploy(WETHToken, "WETHToken", "WETH", 18);
         await deployer.deploy(DAIToken, "DAIToken", "DAI", 18);
 
-        await deployer.deploy(OptionFunction);
+        // "NsureCallToken",
+        //     "CALL",
+        //     address(this),
+        //     underlyingAsset,
+        //     18,
+        //     strikeAsset,
+        //     18,
+        //     _target,
+        //     _deadline
+
+        await deployer.deploy(NsureCallToken, "NsureCallToken", "CALL", XCORE_KOVAN, WETHToken.address, 18, DAIToken.address, 18, "100", "100");
     }
 };
